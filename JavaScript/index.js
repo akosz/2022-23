81f4d488644d3d45151dfdaf52782a654b9cab35
szaboakos <szabo.akos2000@gmail.com>
2022-09-04 20:33:23 +0200

    2022-23

---
+++ JavaScript/index.js
@@ -0,0 +1,74 @@
+/*
+
+***** Áttekintés *****
+
+Kommentek: 
+    egysoros
+    többsoros
+
+Kiírások:
+    alert
+    document.write
+    console.log
+
+Adattípusok:
+    boolean
+    number: nincs külön egész és lebegőpontos
+    string
+    undefined
+    típusellenőrzés (typeof)
+
+Változók
+    var
+    let
+    const
+    konvenciók
+        nem kezdődhet számmal
+        betűt, számot és néhány speciális karaktert tartalmazhat (_, $)
+        foglalt neveket nem használhatunk
+    dinamikusan típusos nyelv
+
+Típuskonverzió
+    +, *
+    Number(), Boolean(), String()
+
+Operátorok
+    aritmetikai: +, -, *, /, %, **
+    hozzárendelő: = , += , -= , *=, /=
+    inkrementáló, dekrementáló: ++, --
+    logikai: &&, ||, !
+    Összehasonlító: ==, ===, !=, !==, <, <=, >, >=
+
+Vezérlési szerkezetek:
+    szelekciós:
+        if, else, else if, switch
+        ternary operátor (?:)
+    ismétléses:
+        while, do-while, for
+
+Függvények:
+    definíció, függ. hívás
+    default paraméter
+    megadási módok:
+        "rendes"
+        anonim
+        arrow 
+        self-invoking
+    
+Tömb:
+    létrehozás
+    hossz
+    indexelés
+    bejárás: for, for-of, for-in
+    tömbkezelő függ.
+
+Object:
+    létrehozás
+    elemek elérése
+    műveletek
+    bejárás: for-in, for-of, for-of
+
+Map
+
+Set
+*/
